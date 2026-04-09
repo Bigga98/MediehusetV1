16f4c3211bd6564d5b1d0704b4fd2db7dc14ab54
Birger Bratli <birgerbratli@Birgers-MacBook-Air.local>
2026-04-09 17:58:43 +0200

    Bytt hei@ til post@mediegruppen.no i alle filer

--- components/navbar.js
+++ components/navbar.js
@@ -475,7 +475,7 @@
               </form>
               <div class="popup-contact-links">
                 <a href="tel:+4748118680" class="popup-contact-link"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.27h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.91a16 16 0 0 0 6.29 6.29l.91-.91a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg>+47 481 18 680</a>
-                <a href="mailto:hei@mediegruppen.no" class="popup-contact-link"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>hei@mediegruppen.no</a>
+                <a href="mailto:post@mediegruppen.no" class="popup-contact-link"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>post@mediegruppen.no</a>
               </div>
             </div>
             <div class="popup-success" id="popupSuccess">
